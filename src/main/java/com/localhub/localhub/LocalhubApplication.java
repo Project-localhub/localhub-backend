e39746e504f338e2dc625a6093c08052b3c249ed
teotaku <dongik9467@naver.com>
2025-12-08 22:04:07 +0900

    first commit

---
+++ src/main/java/com/localhub/localhub/LocalhubApplication.java
@@ -0,0 +1,13 @@
+package com.localhub.localhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/localhub/localhub/LocalhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.localhub.localhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
