359e0dc6632317a3b36261af85efb880d42d6ee5
teotaku <dongik9467@naver.com>
2026-01-08 23:26:14 +0900

    ddl auto 제거

--- src/main/java/com/localhub/localhub/LocalhubApplication.java
+++ src/main/java/com/localhub/localhub/LocalhubApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 
 @SpringBootApplication(
 		exclude = {
 				DataSourceAutoConfiguration.class,
 				DataSourceTransactionManagerAutoConfiguration.class,
-				HibernateJpaAutoConfiguration.class
+				HibernateJpaAutoConfiguration.class,
+				SqlInitializationAutoConfiguration.class
 		}
 )
 public class LocalhubApplication {
