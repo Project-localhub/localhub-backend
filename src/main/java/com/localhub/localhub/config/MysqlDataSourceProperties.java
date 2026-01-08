8b4e2f547e779bddc0b98f2283a0ba20a6355aca
teotaku <dongik9467@naver.com>
2026-01-09 01:11:45 +0900

    1

--- src/main/java/com/localhub/localhub/config/MysqlDataSourceProperties.java
+++ src/main/java/com/localhub/localhub/config/MysqlDataSourceProperties.java
@@ -1,12 +1,13 @@
 package com.localhub.localhub.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "mysql.datasource")
-@Getter
+@Getter @Setter
 public class MysqlDataSourceProperties {
     private String url;
     private String username;
