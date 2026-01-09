b13ca9511a5edaa50308a9bfa44e920f2e48ae5f
teotaku <dongik9467@naver.com>
2026-01-09 13:11:33 +0900

    1

--- src/main/java/com/localhub/localhub/entity/RefreshEntity.java
+++ src/main/java/com/localhub/localhub/entity/RefreshEntity.java
@@ -1,13 +1,11 @@
 package com.localhub.localhub.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
 @Entity
+@Table(name = "refresh_entity")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
